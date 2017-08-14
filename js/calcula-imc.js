4da0d1a5c587357f0995fffcf967b891a854e5e6
anacarolineo <anacarolineo@execplan.com.br>
2017-08-14 18:51:26 -0300

    Cont do curso de JS - Alura

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -10,30 +10,48 @@ for (var lintCont = 0; lintCont < paciente.length; lintCont++) {
     var tdAltura = paciente[lintCont].querySelector(".info-altura");
     var altura = tdAltura.textContent;
     var tdImc =  paciente[lintCont].querySelector(".info-imc");
-    var pesoEhValido = true;
-    var alturaEhValida = true;
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
 
-    if (peso <= 0 || peso >= 1000) {
+    if (!pesoEhValido) {
         console.log("Peso inválido!");
         pesoEhValido = false;
-        tdImc.textContent = "Peso inválido!";
-        paciente[lintCont].classList.add('paciente-invalido')
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
     }
-
-    if (altura <= 0 || altura >= 3.00) {
+    
+    if (!alturaEhValida) {
         console.log("Altura inválida!");
         alturaEhValida = false;
-        tdImc.textContent = "Altura inválida!";
-        paciente[lintCont].classList.add('paciente-invalido')
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
     }
 
     if (alturaEhValida && pesoEhValido) {
         var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
+        tdImc.textContent = calculaImc(peso, altura);
     }
 }
 
-botaoAdicionar.addEventListener("click", function(event) {
-    event.preventDefault();
-    console.log("Oi, cliquei no botão.");
-});
+function calculaImc(peso, altura) {
+    var imc = 0;
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+}
+
+function validaPeso(peso) {
+    if (peso > 0 && peso < 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validaAltura(altura) {    
+    if (altura > 0 && altura < 3.00) {
+        return true;
+    } else {
+        return false;
+    }
+}
